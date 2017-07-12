b0493d2c8ae4e84f17d9a68ad36e3e79ef44c024
VARTIKA SINGH <vrsingh@grepruby.com>
2017-07-12 15:19:29 +0530

    Added assignment3_1

---
+++ assignment3/assignment3_main.js
@@ -0,0 +1,11 @@
+var second_largest = require("./assignment1_1.js");
+var calculate_frequency = require("./assignment1_2.js");
+var unflattenFunction = require("./assignment1_3.js");
+var array=[1,4,6,8];
+var second=second_largest.secondLargest(array);
+console.log(second);
+
+
+
+
+
