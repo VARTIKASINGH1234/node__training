057a51ca99daddda5245a7bff88128d3387607a7
VARTIKA SINGH <vrsingh@grepruby.com>
2017-07-21 15:05:33 +0530

    Added employee controller

--- assignment8/controllerEmployee.js
+++ assignment8/controllerEmployee.js
@@ -1,23 +1,22 @@
 
 const mongoose = require('mongoose');
 
-const employee = require('./employee');
+const Employee = require('./employee');
 
 exports.addEmployee = function (req, res) {
-  let emplo = new employee();
-  emplo.employeeId = req.body.employeeId;
-  emplo.employeeName = req.body.employeeName;
-  emplo.employeeAddress = req.body.employeeAddress;
-  emplo.reportingManagerId = req.body.reportingManagerId;
-  emplo.save(function (error) {
+  let employee = new Employee();
+  employee.employeeName = req.body.employeeName;
+  employee.employeeAddress = req.body.employeeAddress;
+  employee.reportingManagerId = req.body.reportingManagerId;
+  employee.save(function (error) {
     if (error) {
       res.send(error);
     }
-    res.send({ message: 'Employee Created !' });
+    res.send({"message": "employee is created"});
   });  
 }
 exports.getEmployee = function (req, res) {
-  product.find(function (err, employees) {
+  Employee.find({}, function (err, employees) {
     if (err) {
       res.send(err);
     }
@@ -26,21 +25,22 @@ exports.getEmployee = function (req, res) {
 }
 
 exports.getEmployeeId = function (req, res) {
-  product.findById(req.params.employeeId, function (err, emplo) {
+  Employee.findById(req.params.employeeId, function (err, employee) {
     if (err)
       res.send(err);
-    res.json(emplo);
+    res.json(employee);
   });
 }
 
 exports.updateEmployee = function (req, res) {
-  product.findById(req.params.projectId, function (err, emplo) {
+  Employee.findById(req.params.ObjectId, function (err, employee) {
     if (err) {
       res.send(err);
     }
-    p.employeeId = req.body.employeeId;
-    p.reportingManagerId = req.body.reportingManagerId;
-    project.save(function (err) {
+    employee.employeeName = req.body.employeeName;
+    employee.employeeAddress = req.body.employeeAddress;
+    employee.reportingManagerId = req.body.reportingManagerId;
+    employee.save(function (err) {
       if (err)
         res.send(err);
       res.json({ message: 'Product updated!' });
